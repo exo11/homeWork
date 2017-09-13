dfaad859bdaba4265295f7a502047fd1e3a65ced
Kirill Abazovsky <0.7408oz@gmail.com>
2017-09-13 14:41:03 +0300

    homeWork_5(1)

--- xhr/books-catalog/js/books-catalog.js
+++ xhr/books-catalog/js/books-catalog.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const xhr = new XMLHttpRequest(),
-    content = document.getElementById('content');
+  content = document.getElementById('content');
 
 xhr.open("GET", "https://netology-fbb-store-api.herokuapp.com/book/");
 xhr.send();
@@ -9,17 +9,17 @@ xhr.send();
 xhr.addEventListener('load', addBooks);
 
 function addBooks() {
-    const books = JSON.parse(xhr.responseText);
-    let str = '';
-    for (let book of books) {
-        str +=
-        `<li
-  			    data-title="${book.title}"
-  				  data-author="${book.author.name}"
-  				  data-info="${book.info}"
-  				  data-price="${book.price}">
-  				  <img src="${book.cover.small}">
-		    </li>`
-    }
-    content.innerHTML = str;
+  const books = JSON.parse(xhr.responseText);
+
+  content.innerHTML = books.reduce((sum, book) => {
+    return sum + 
+      `<li
+			  data-title="${book.title}"
+				data-author="${book.author.name}"
+				data-info="${book.info}"
+				data-price="${book.price}">
+				<img src="${book.cover.small}">
+			</li>`
+    }, '');
+
 }
--- xhr/tabs/js/tabs.js
+++ xhr/tabs/js/tabs.js
@@ -1,29 +1,34 @@
 'use strict';
 
 const links = document.querySelectorAll('nav > a'),
-    content = document.getElementById('content'),
-    preloader = document.getElementById('preloader'),
-    xhr = new XMLHttpRequest();
+  content = document.getElementById('content'),
+  preloader = document.getElementById('preloader'),
+  xhr = new XMLHttpRequest();
 
 loadText(links[0]);
 
 for (let link of links) {
 
-    link.addEventListener('click', event => {
-        event.preventDefault();
-        preloader.classList.remove('hidden');
-        links.forEach(link => link.classList.remove('active'));
-        link.classList.add('active');
-        loadText(link);
-    });
+  link.addEventListener('click', event => {
+    event.preventDefault();
+
+    if (!(link.classList.contains('active'))) {
+      content.innerHTML = null;
+      preloader.classList.remove('hidden');
+      links.forEach(link => link.classList.remove('active'));
+      link.classList.add('active');
+      loadText(link);
+    }
+
+  });
 
 }
 
 function loadText(link) {
-    xhr.open("GET", link.getAttribute('href'));
-    xhr.send();
-    xhr.addEventListener('load', () => {
-        content.innerHTML = xhr.responseText;
-        preloader.classList.add('hidden');
-    });
+  xhr.open("GET", link.getAttribute('href'));
+  xhr.send();
+  xhr.addEventListener('load', () => {
+    content.innerHTML = xhr.responseText;
+    preloader.classList.add('hidden');
+  });
 }
