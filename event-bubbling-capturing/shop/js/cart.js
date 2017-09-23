abbaf5d5a4ac5d2e7085c3833927427b1ec8ddd9
Kirill Abazovsky <0.7408oz@gmail.com>
2017-09-23 20:32:00 +0300

    homeWork_8(1)

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -4,17 +4,14 @@ const listItem = document.querySelector('.items-list');
 
 listItem.addEventListener('click', handlerAddToCart);
 
+
 function handlerAddToCart(event) {
-  
+  event.preventDefault();
   if (event.target.classList.contains('add-to-cart')) {
-    
     let item = {
       title: event.target.dataset.title,
       price: event.target.dataset.price
     };
-
     addToCart(item);
-  
   }
-
 }
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,16 +1,12 @@
 'use strict';
 
 function handleTableClick(event) {
-  
   let evTarDat = event.target.dataset;
-
   if (event.target.classList.contains('prop__name')) {
     table.setAttribute('data-sort-by', evTarDat.propName);
-
     if (evTarDat.dir === undefined) {
       event.target.setAttribute('data-dir', 1);
     }
-
     if (evTarDat.dir === '1') {
       sortTable(table.dataset.sortBy, evTarDat.dir);
       evTarDat.dir = -1;
@@ -18,7 +14,5 @@ function handleTableClick(event) {
       sortTable(table.dataset.sortBy, evTarDat.dir);
       evTarDat.dir = 1;
     }
-
   }
-
 }
