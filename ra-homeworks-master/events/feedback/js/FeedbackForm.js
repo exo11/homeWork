5c4e83dd26653518e2b177e38443997d14a19108
Kirill Abazovsky <0.7408oz@gmail.com>
2017-11-09 12:01:27 +0300

    react_Events_1

--- ra-homeworks-master/events/feedback/js/FeedbackForm.js
+++ ra-homeworks-master/events/feedback/js/FeedbackForm.js
@@ -4,18 +4,7 @@ function FeedbackForm(props) {
   let {salutation, name, subject, message, email, snacks} = props.data,
     form;
   
-  {/*function addDefaultCheck() {
-    const inputs = form
-      .querySelectorAll('.contact-form__input-group input');
-    inputs.forEach(input => {
-      if (salutation === input.value || snacks[0] === input.value) {
-        input.defaultChecked = true;
-      }
-    });
-  } 
-  window.onload = addDefaultChecked();*/}
   
-
   function submitHandler(event) {
     event.preventDefault();
     if (typeof props.onSubmit !== 'function') {
@@ -34,6 +23,17 @@ function FeedbackForm(props) {
     });
     props.onSubmit(fieldValues);
   }
+
+
+  function isCheckedSalutation(value) {
+    return salutation === value;
+  }
+
+  
+  function isCheckedSnacks(value) {
+    return snacks[0] === value;
+  }
+
   
   return (
     <form className="content__form contact-form" onSubmit={submitHandler}  ref={el => form = el}>
@@ -41,11 +41,11 @@ function FeedbackForm(props) {
         <p>Чем мы можем помочь?</p>
       </div>
       <div className="contact-form__input-group">
-        <input className="contact-form__input contact-form__input--radio" id="salutation-mr" name="salutation" type="radio" value="Мистер" defaultChecked={salutation === 'Мистер'}/>
+        <input className="contact-form__input contact-form__input--radio" id="salutation-mr" name="salutation" type="radio" value="Мистер" defaultChecked={isCheckedSalutation('Мистер')}/>
         <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mr">Мистер</label>
-        <input className="contact-form__input contact-form__input--radio" id="salutation-mrs" name="salutation" type="radio" value="Мисис" defaultChecked={salutation === 'Мисис'}/>
+        <input className="contact-form__input contact-form__input--radio" id="salutation-mrs" name="salutation" type="radio" value="Мисис" defaultChecked={isCheckedSalutation('Мисис')}/>
         <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mrs">Мисис</label>
-        <input className="contact-form__input contact-form__input--radio" id="salutation-ms" name="salutation" type="radio" value="Мис" defaultChecked={salutation === 'Мис'}/>
+        <input className="contact-form__input contact-form__input--radio" id="salutation-ms" name="salutation" type="radio" value="Мис" defaultChecked={isCheckedSalutation('Мис')}/>
         <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-ms">Мис</label>
       </div>
       <div className="contact-form__input-group">
@@ -69,9 +69,9 @@ function FeedbackForm(props) {
       </div>
       <div className="contact-form__input-group">
         <p className="contact-form__label--checkbox-group">Хочу получить:</p>
-        <input className="contact-form__input contact-form__input--checkbox" id="snacks-pizza" name="snacks" type="checkbox" value="пицца" defaultChecked ={snacks[0] === 'пицца'}/>
+        <input className="contact-form__input contact-form__input--checkbox" id="snacks-pizza" name="snacks" type="checkbox" value="пицца" defaultChecked={isCheckedSnacks('пицца')}/>
         <label className="contact-form__label contact-form__label--checkbox" htmlFor="snacks-pizza">Пиццу</label>
-        <input className="contact-form__input contact-form__input--checkbox" id="snacks-cake" name="snacks" type="checkbox" value="пирог" defaultChecked ={snacks[0] === 'пирог'}/>
+        <input className="contact-form__input contact-form__input--checkbox" id="snacks-cake" name="snacks" type="checkbox" value="пирог" defaultChecked={isCheckedSnacks('пирог')}/>
         <label className="contact-form__label contact-form__label--checkbox" htmlFor="snacks-cake">Пирог</label>
       </div>
       <button className="contact-form__button" type="submit" >Отправить сообщение!</button>
